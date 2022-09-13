5e895ca0d44ee974cdab6c1974ff3ecb017cf294
pool <loop.edward@gmail.com>
2022-09-13 09:56:09 +0800

    Create stub lambda function

--- project_alvarez/project_alvarez_stack.py
+++ project_alvarez/project_alvarez_stack.py
@@ -1,19 +1,22 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
+
 from constructs import Construct
+
 
 class ProjectAlvarezStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        my_lambda = _lambda.Function(self, 'HelloHandler',
+                                     runtime=_lambda.Runtime.PYTHON_3_8,
+                                     code=_lambda.Code.from_asset(
+                                         'project_alvarez/lambda'),
+                                     handler='hello.handler')
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ProjectAlvarezQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        apigw.LambdaRestApi(self, 'Endpoint',
+                            handler=my_lambda)
